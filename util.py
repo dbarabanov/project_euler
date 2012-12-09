0178b3901978d6fbf7ee7959df8df72b437f3e6d
Dmitri Barabanov <dmitri@brooklynpacket.com>
2012-12-09 00:10:21 -0800

    adding is_square() to util

--- util.py
+++ util.py
@@ -1,6 +1,16 @@
 import math
 from datetime import datetime
 
+def is_square(n):
+    if n < 0: return False
+    if n == 0 or n == 1 : return True 
+    x = n // 2
+    seen = set([x])
+    while x*x != n:
+        x = (x+(n//x))//2
+        if x in seen: return False
+        seen.add(x)
+    return True
 
 class Helper:
     primes = []
